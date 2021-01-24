ae821a619ef2d3cdc708fbea60efacb59bf1093d
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2021-01-24 17:15:00 +0900

    lesson7-1の追加

---
+++ lesson7-1/lesson7-1.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+using namespace std;
+
+int square(int x) {
+	return x * x;
+}
+
+int main(void) {
+	int a;
+	cout << "整数を入力してください" << endl;
+	cin >> a;
+	cout << a << "の2乗は" << square(a) << "です。" << endl;
+}
