7a9d7a74948edc93cc2a983b5504b98ca8958b7d
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2021-01-24 16:55:14 +0900

    lesson6-2追加

---
+++ lesson6-2/lesson6-2.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+using namespace std;
+
+int main(void){
+	int a, sum = 0;
+	cout << "テストの点数を入力してください。" << endl;
+	do {
+		cin >> a;
+		sum += a;
+	} while (a);
+	cout << "テストの合計点は" << sum << "点です。" << endl;
+}
