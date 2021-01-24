fb12ede4b92f9d2ba0c9857d4e1fe41e552a201a
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2021-01-24 16:45:26 +0900

    プロジェクト ファイルを追加します。

---
+++ テスト勉強/lesson6-1.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+using namespace std;
+
+int main(void) {
+	cout << "1から10までの偶数を出力します。" << endl;
+	for (int i = 1; i <= 10; i++) {
+		if (!(i % 2)) {
+			cout << i << endl;
+		}
+	}
+	return 0;
+}
