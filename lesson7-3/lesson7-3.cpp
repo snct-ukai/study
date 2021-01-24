ce3aa5e68e5eb449e40ef53edae4064b3ba8cba6
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2021-01-24 17:22:01 +0900

    lesson7-3の追加

---
+++ lesson7-3/lesson7-3.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+using namespace std;
+
+inline int square(int x) {
+	return x * x;
+}
+
+inline double square(double x) {
+	return x * x;
+}
+
+int main(void) {
+	int a;
+	double b;
+	cout << "整数を入力してください" << endl;
+	cin >> a;
+	cout << a << "の2乗は" << square(a) << "です。" << endl;
+	cout << "少数を入力してください。" << endl;
+	cin >> b;
+	cout << b << "の2乗は" << square(b) << "です。" << endl;
+}
