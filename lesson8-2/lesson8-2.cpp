41edc31b67a6ffc153d41807f7fa5491c85aa7ab
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2021-01-24 17:35:59 +0900

    lesson8-2の追加

---
+++ lesson8-2/lesson8-2.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+using namespace std;
+
+void add(int* x, int* y, int a) {
+	*x += a;
+	*y += a;
+}
+
+int main(void){
+	int x, y, a;
+	cout << "2科目分の点数を入力してください。" << endl;
+	cin >> x >> y;
+	cout << "加算する点数を入力してください。" << endl;
+	cin >> a;
+	cout << a << "点加算しましたので" << endl;
+	add(&x, &y, a);
+	cout << "科目1は" << x << "点となりました。" << endl;
+	cout << "科目2は" << y << "点となりました。" << endl;
+}
